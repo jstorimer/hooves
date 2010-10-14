c9a58d6d6ee9f8bd406adc05ec165956fc2a2ee1
Jesse Storimer <jstorimer@gmail.com>
2010-10-14 05:45:34 -0400

    Add support for unicorn config files

--- README.md
+++ README.md
@@ -6,6 +6,20 @@ Install
 --------
 It's a rubygem, you'll want to include it in your Gemfile. 
 
+Usage
+------
+Just use `rails server`.
+
+Config file
+------------
+Hooves looks for unicorn config files in two locations.
+
+First it looks in ~/.hooves. If that file exists then it gets passed to Unicorn as the config file.
+
+Second it looks in ./.hooves.
+
+We look in ~/ first because you can only pass one config file to Unicorn. If your project has a .hooves file checked in to its root and you want to have a custom config file, that might be a pain. So we look in ~/ first. That might also become a pain if you want to have different configs for different projects. Let me know and we'll find something that works.
+
 Caveats
 --------
 * Using this gem monkey patches Rack::Handler.default making Unicorn the default.
--- lib/hooves/unicorn.rb
+++ lib/hooves/unicorn.rb
@@ -5,19 +5,51 @@ require "rack/chunked"
 module Hooves
   # Rack Handler to use Unicorn for Rails::Application.run!
   module Unicorn
-    def self.run(app, options={})
-      options[:listeners] = ["#{options.delete(:Host)}:#{options.delete(:Port)}"]
-      options[:worker_processes] = 3
-      $DEBUG = true if options.delete(:debugger)
-      daemonize = options.delete(:daemonize)
+    class << self
+      def run(app, options={})
+        options[:listeners] = ["#{options.delete(:Host)}:#{options.delete(:Port)}"]
+        options[:worker_processes] = 3
 
-      # :pid option is stronly discouraged except in unicorn config file
-      uni_options = options.slice(:listeners, :worker_processes)
-      
-      if daemonize
-        ::Unicorn::Launcher.daemonize!(uni_options)
-      else
-        ::Unicorn.run(app, uni_options)
+        if options.delete(:debugger)
+          $DEBUG = true
+
+          # It's difficult to debug with more than one worker on the go
+          options[:worker_processes] = 1
+          # The default timeout (30s) doesn't leave much time for debugging
+          options[:timeout] = 120
+        end
+
+        daemonize = options.delete(:daemonize)
+
+        # override any settings that you want in your config file
+        options[:config_file] = find_config_file
+
+        # :pid option is stronly discouraged except in unicorn config file
+        uni_options = options.slice(:listeners, :worker_processes, :config_file, :timeout)
+
+        if daemonize
+          ::Unicorn::Launcher.daemonize!(uni_options)
+        else
+          ::Unicorn.run(app, uni_options)
+        end
+      end
+
+      USER_CONFIG = File.expand_path('~/.hooves')
+      PROJECT_CONFIG = File.expand_path('./.hooves')
+      def find_config_file
+        if user_config?
+          USER_CONFIG
+        elsif project_config?
+          PROJECT_CONFIG
+        end
+      end
+
+      def user_config?
+        File.exist?(USER_CONFIG)
+      end
+
+      def project_config?
+        File.exist?(PROJECT_CONFIG)
       end
     end
   end
